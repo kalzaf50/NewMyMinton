73b99d2e2abf6c8a8b23b84b9f43ee3d58ac3c6c
Kalzaf <kalzaf50@gmail.com>
2025-07-17 22:58:49 +0800

    cuba try

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
 
+// lmao
 mongoose.connect(process.env.MONGODB_ENDPOINT)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB connection error:', err));
