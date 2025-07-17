c1d932095cc5faa2547cdac5845b0877930bf604
Kalzaf <kalzaf50@gmail.com>
2025-07-17 22:05:02 +0800

    Push all

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+import 'dotenv/config'
+import mongoose from 'mongoose'
 
-mongoose.connect('mongodb+srv://myminton_user:yourPassword123@mymintoncluster.vioenjg.mongodb.net/MyMintonDB?retryWrites=true&w=majority&appName=MyMintonCluster')
+mongoose.connect(process.env.MONGODB_ENDPOINT)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
